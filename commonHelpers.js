51d2e0c28fb203c4c3eeb9506c32bf6bfc3b4df5
AiHikki <140841723+AiHikki@users.noreply.github.com>
2024-01-04 18:02:13 +0000

    Deploying to gh-pages from @ AiHikki/goit-js-hw-11@477c91c69c64ad91cae261bd738c612edeef0cd5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,26 @@
-import{i as f,S as m}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function h(){f.show({close:!1,closeOnClick:!0,message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",timeout:3e3,transitionIn:"flipInX",transitionOut:"flipOutX",position:"topRight",backgroundColor:"red",progressBar:!1})}let p=new m("#gallery a",{overlayOpacity:.5,showCounter:!1});const l=document.querySelector("#form"),g=document.querySelector("#searchInput"),c=document.querySelector("#gallery"),a=document.querySelector(".loader");l.addEventListener("submit",y);function y(i){a.classList.remove("hide"),c.innerHTML="",i.preventDefault();const o=new URLSearchParams({key:"41474300-2fa05bee877be877b8dc1781f",q:g.value,orientation:"horizontal",image_type:"photo",safesearch:!0});fetch(`https://pixabay.com/api/?${o}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{setTimeout(()=>{if(a.classList.add("hide"),r.hits.length===0)return h();v(r.hits)},2e3)}).catch(r=>console.log(r)).finally(()=>{l.reset()})}function v(i){c.innerHTML=i.reduce((o,{webformatURL:r,largeImageURL:n,tags:e,likes:t,views:s,comments:u,downloads:d})=>o+`
+import{i as m,S as f}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&t(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerpolicy&&(i.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?i.credentials="include":e.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function t(e){if(e.ep)return;e.ep=!0;const i=s(e);fetch(e.href,i)}})();function h(){m.show({close:!1,closeOnClick:!0,message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",timeout:3e3,transitionIn:"flipInX",transitionOut:"flipOutX",position:"topRight",backgroundColor:"red",progressBar:!1})}let g=new f("#gallery a",{overlayOpacity:.5,showCounter:!1});const n=document.querySelector("#form"),v=document.querySelector("#searchInput"),c=document.querySelector("#gallery"),l=document.querySelector(".loader");n.addEventListener("submit",y);function y(a){l.classList.remove("hide"),c.innerHTML="",a.preventDefault();const r=new URL("https://pixabay.com/api/"),s=new URLSearchParams({key:"41474300-2fa05bee877be877b8dc1781f",q:v.value,orientation:"horizontal",image_type:"photo",safesearch:!0});fetch(`${r}?${s}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{setTimeout(()=>{if(l.classList.add("hide"),t.hits.length===0)return h();p(t.hits)},2e3)}).catch(t=>console.log(t)).finally(()=>{n.reset()})}function p(a){c.innerHTML=a.reduce((r,{webformatURL:s,largeImageURL:t,tags:e,likes:i,views:o,comments:d,downloads:u})=>r+`
       <li class="gallery-item">
-        <a href="${n}">
-          <img src="${r}" alt="${e}" />
+        <a href="${t}">
+          <img src="${s}" alt="${e}" />
         </a>
         <div class="image-desc">
-          <div>Likes <span>${t}</span></div>
-          <div>Views <span>${s}</span></div>
-          <div>Comments <span>${u}</span></div>
-          <div>Downloads <span>${d}</span></div>
+          <div class="image-desc-item">
+            <div class="image-desc-label">Likes</div>
+            <div>${i}</div>
+          </div>
+          <div class="image-desc-item">
+             <div class="image-desc-label">Views</div>
+             <div>${o}</div>
+          </div>
+          <div class="image-desc-item">
+            <div class="image-desc-label">Comments</div>
+            <div>${d}</div>
+          </div>
+          <div class="image-desc-item">
+            <div class="image-desc-label">Downloads</div>
+            <div>${u}</div>
+          </div>
         </div>
       </li>
-      `,""),p.refresh()}
+      `,""),g.refresh()}
 //# sourceMappingURL=commonHelpers.js.map
